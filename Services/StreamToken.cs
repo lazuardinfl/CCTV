a3578576c56dd0e72989f98939f29953654af126
Lazuardi Naufal <lazuardi.n@samsung.com>
2025-01-08 08:41:32 +0700

    add stream token validator

--- Services/StreamToken.cs
+++ Services/StreamToken.cs
@@ -6,17 +6,11 @@ using System.Text;
 
 namespace CCTV.Services;
 
-public class StreamToken
+public class StreamToken(IConfiguration configuration, ILogger<StreamToken> logger)
 {
-    private readonly ILogger logger;
-    private readonly SecurityKey key;
-
-    public StreamToken(IConfiguration configuration, ILogger<StreamToken> logger)
-    {
-        key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["STREAM_SECRET"] ??
-            RandomNumberGenerator.GetString("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", 512)));
-        this.logger = logger;
-    }
+    private readonly ILogger logger = logger;
+    private readonly SecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["STREAM_SECRET"] ??
+        RandomNumberGenerator.GetString("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", 512)));
 
     public string GenerateToken(string cctv, DateTime expires)
     {
@@ -29,5 +23,20 @@ public class StreamToken
             SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
         };
         return tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
+    }
+
+    public async Task<bool> ValidateToken(string token)
+    {
+        logger.LogInformation("token validated");
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var validationParameters = new TokenValidationParameters {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = key,
+            ClockSkew = TimeSpan.Zero
+        };
+        var result = await tokenHandler.ValidateTokenAsync(token, validationParameters);
+        return result.IsValid;
     }
 }
