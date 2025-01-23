de0d5bbbf1ccb5a31d7411fba50e8323cff9b654
Lazuardi Naufal <lazuardi.n@samsung.com>
2025-01-23 11:31:47 +0700

    fix federated logout from keycloak

--- ClientApp/lib/auth.ts
+++ ClientApp/lib/auth.ts
@@ -5,6 +5,7 @@ import Keycloak from "@auth/sveltekit/providers/keycloak";
 declare module "@auth/core/jwt" {
     interface JWT {
         accessToken?: string | null
+        idToken?: string | null
     }
 }
 
@@ -13,11 +14,23 @@ export const { handle, signIn, signOut } = SvelteKitAuth({
     trustHost: true,
     providers: [Keycloak],
     callbacks: {
-        async jwt({ token, account }) {
+        async jwt({ account, token }) {
             if (account) {
                 token.accessToken = account.access_token;
+                token.idToken = account.id_token;
             }
             return token;
+        }
+    },
+    events: {
+        async signOut(message) {
+            const queries = new URLSearchParams({
+                "client_id": env.AUTH_KEYCLOAK_ID,
+                "id_token_hint": (message as any).token.idToken
+            });
+            await fetch(`${env.AUTH_KEYCLOAK_ISSUER}/protocol/openid-connect/logout?${queries.toString()}`, {
+                method: "GET"
+            });
         }
     }
 });
